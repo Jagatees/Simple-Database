184697ed517aa21a1947a3d48885cad8f160a718
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-10-25 00:24:06 +0800

    Get Useriput

--- EzDB/EzDB/main.c
+++ EzDB/EzDB/main.c
@@ -5,10 +5,50 @@
 //  Created by Jagateesvaran on 24/10/23.
 //
 
+
 #include <stdio.h>
 
+
+/*
+ * Get the name of the chatbot.
+ *
+ * Returns: the name of the chatbot as a null-terminated string
+ */
+
+
+#define MAX_USER_INPUT 256
+
+
+// Get user input
+// check is it is empty first
+// check what is the intent
+// send to the write function to handle the write intent
+// if not then return false say wrong entry
+// user enter exit can exit the databse
+// 1 = keep looping , 0 stop looping
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    char user_input[MAX_USER_INPUT];
+    int inDatabase = 0;
+    
+    /* print a welcome message */
+    
+    printf("Welcome to Hakari Dance Database .\n");
+
+    
+    do {
+        
+        printf("Enter Somthing :");
+        // fgets get mutiple string in a 
+        fgets(user_input, MAX_USER_INPUT, stdin);
+        printf("What user entered %s", user_input);
+
+    
+        inDatabase = 1; // 1 == break loop , 0 == keep looping
+    } while (!inDatabase);
+    
+    
     return 0;
 }
