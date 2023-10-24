f1898751b6e263bf0a559dfa79bf37f17236a028
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-10-24 08:47:44 +0800

    init project

---
+++ EzDB/EzDB/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  EzDB
+//
+//  Created by Jagateesvaran on 24/10/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
