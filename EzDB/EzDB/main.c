610f387f335f51bca45ca4bb722c7324fb2cbf9b
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-10-25 21:50:33 +0800

    Checkpoint

--- EzDB/EzDB/main.c
+++ EzDB/EzDB/main.c
@@ -9,55 +9,65 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "utility.h"
 #include "database.h"
 
+
+
+
 int main(int argc, const char * argv[]) {
     
-    // Get user input , 
-    // max number of string a user can enter in the line
-    char user_input[MAX_USER_INPUT];
-    int inDatabase = 0;
-    // rename this variable
-    int inc = 0;
-    char *inv[MAX_USER_INPUT];
+  
     
     
-    printf("Welcome to EzDB\n");
     
-    do {
-        // Display Text Message
-        printf("Enter Query :");
-        // Get a string os user input
-        fgets(user_input, MAX_USER_INPUT, stdin);
-        
-        
-        // conver to upper case
-        for (int i = 0; user_input[i]; i++) {
-            user_input[i] = toupper((unsigned char)user_input[i]);
-        }
-        
-        printf("Uppercase: %s", user_input);
-
-        
-        // still not sure it like remove stuff from the string and store in inv[0] ?
-        inc = 0;
-        inv[inc] = strtok(user_input, characters);
-
-        // Split String
-        // Place a NULL as a last value
-        // break from there
-        while (inv[inc] != NULL) {
-            inc++;
-            inv[inc] = strtok(NULL, characters);
-        }
-        
-        // retturn (1 == break loop , 0 = keep looping)
-        inDatabase = logic_databse(inv, inc);
-        
-    } while (!inDatabase);
-   
+    
+    
+//    // Get user input , 
+//    // max number of string a user can enter in the line
+//    char user_input[MAX_USER_INPUT];
+//    int inDatabase = 0;
+//    // rename this variable
+//    int inc = 0;
+//    char *inv[MAX_USER_INPUT];
+//    
+//    
+//    printf("Welcome to EzDB\n");
+//    
+//    do {
+//        // Display Text Message
+//        printf("Enter Query :");
+//        // Get a string os user input
+//        fgets(user_input, MAX_USER_INPUT, stdin);
+//        
+//        
+//        // conver to upper case
+//        for (int i = 0; user_input[i]; i++) {
+//            user_input[i] = toupper((unsigned char)user_input[i]);
+//        }
+//        
+//        printf("Uppercase: %s", user_input);
+//
+//        
+//        // still not sure it like remove stuff from the string and store in inv[0] ?
+//        inc = 0;
+//        inv[inc] = strtok(user_input, characters);
+//
+//        // Split String
+//        // Place a NULL as a last value
+//        // break from there
+//        while (inv[inc] != NULL) {
+//            inc++;
+//            inv[inc] = strtok(NULL, characters);
+//        }
+//        
+//        // retturn (1 == break loop , 0 = keep looping)
+//        inDatabase = logic_databse(inv, inc);
+//        
+//    } while (!inDatabase);
+//   
     return 0;
 }
 
