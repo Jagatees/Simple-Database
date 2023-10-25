ebfdc43fbbcf34db114767645605144b634e3bcd
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-10-25 22:26:04 +0800

    Starting to Learn Linked List

--- EzDB/EzDB/main.c
+++ EzDB/EzDB/main.c
@@ -15,12 +15,44 @@
 #include "database.h"
 
 
+struct node {
+    int value;
+    struct node* next;
+};
+
+typedef struct node node_t;
+
+
+void printlist(node_t *head) {
+    node_t *temporary = head;
+    
+    while (temporary != NULL) {
+        printf("%d - ", temporary->value);
+        temporary = temporary->next;
+    }
+    
+    printf("\n");
+}
+
 
 
 int main(int argc, const char * argv[]) {
     
-  
+    node_t n1,n2,n3;
+    node_t *head;
     
+    n1.value = 1;
+    n2.value = 2;
+    n3.value = 3;
+    
+    head = &n2;
+    
+    n1.next = &n3;
+    n2.next = &n1;
+    n3.next = NULL;
+    
+  
+    printlist(head);
     
     
     
