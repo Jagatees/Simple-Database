5b906e6ecffc4f05a8ab646a1ebab72f88045d7a
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-11-04 12:47:55 +0800

    Cleaning code

--- EzDB/EzDB/main.c
+++ EzDB/EzDB/main.c
@@ -1,60 +1,60 @@
-//
-//  main.c
-//  EzDB
-//
-//  Created by Jagateesvaran on 24/10/23.
-//
 
-// C Libary
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <stdlib.h>
+// Include C Header Files
+#include <stdio.h> // input-output
+#include <ctype.h> // Character Type
+#include <string.h> // String Manipulation
+#include <stdlib.h> // Standard Library
 
-#include "utility.h"
-#include "database.h"
-#include "j_node.h"
+// Include Custom Header Files
+#include "utility.h"  // Store General Variable
+#include "database.h" // Structure of Database
+#include "j_node.h"   // Structure of Linked List
+
+
 
 int main(int argc, const char *argv[])
 {
     
+    // init linked head to null on start
     node_t *head = NULL;
     
-    // Get user input ,
-    // max number of string a user can enter in the line
+    // Store user input
     char user_input[MAX_USER_INPUT];
+    
+    // Database Loop (think of it like a game loop) , 0 == still in database , 1 == will exit database
     int inDatabase = 0;
-    // rename this variable
-    int inc = 0;
-    char *inv[MAX_USER_INPUT];
+    
+    // Split User Input
+    int counter = 0;
+    char *split_user_input[MAX_USER_INPUT];
 
+    // Print Welcome Message
     printf("-------Welcome to EzDB-------\n");
 
+    // Database Loop
     do
     {
-        printf("\n");
-        // Display Text Message
+        // Print Input Line
         printf("Enter INSTRUCTION :");
-        // Get a string os user input
+        // store the whole line of test user enter
         fgets(user_input, MAX_USER_INPUT, stdin);
        
-
-
-        // still not sure it like remove stuff from the string and store in inv[0] ?
-        inc = 0;
-        inv[inc] = strtok(user_input, characters);
+        // init counter to zero
+        // 
+        counter = 0;
+        split_user_input[counter] = strtok(user_input, characters);
 
         // Split String
         // Place a NULL as a last value
         // break from there
-        while (inv[inc] != NULL)
+        while (split_user_input[counter] != NULL)
         {
-            inc++;
-            inv[inc] = strtok(NULL, characters);
+            counter++;
+            split_user_input[counter] = strtok(NULL, characters);
         }
 
-        // retturn (1 == break loop , 0 = keep looping)
-        inDatabase = logic_database(inv, inc, &head);
+        
+        inDatabase = logic_database(split_user_input, counter, &head);
 
     } while (!inDatabase);
 
