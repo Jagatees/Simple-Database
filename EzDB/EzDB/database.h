8be4de822bd97153261cd1c5ac3f58d118ac0c87
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-11-08 23:28:18 +0800

    Build Ready For testing

--- EzDB/EzDB/database.h
+++ EzDB/EzDB/database.h
@@ -26,6 +26,25 @@
 
 #include "database.h"
 
+const int MAX_INSTRUCTION = 10;
+const char *FILE_EXTENSION = ".txt";
+const char *FILE_NOT_FOUND = "Unable to find %s at location\n";
+const char *DB_INSTRUCTION[MAX_INSTRUCTION] = {
+    "SHOW", "ALL", "INSERT", "QUERY", "UPDATE", "DELETE", "OPEN", "SAVE", "EXIT", "HELP",
+};
+
+typedef enum {
+    SHOW, ALL, INSERT, QUERY, UPDATE, DELETE, OPEN, SAVE, EXIT, HELP, UNKNOWN_COMMAND
+} CommandType;
+
+
+typedef enum {
+    db_Close,
+    db_Open
+} DBState;
+
+
+CommandType getCommandType(const char *user_input);
 
 void switch_state(const char* user_input);
 
@@ -34,14 +53,13 @@ int databaseLogic(char *inv[], int inc, node_t **head);
 void handleOpenCommand(char *inv[], node_t **head);
 void handleShowAllCommand(char *inv[], int inc, node_t **head);
 void handleExitCommand(char *inv[]);
-void handleHelpCommand(DBState db_state);
 void handleInsertCommand(char *inv[], node_t **head);
 void handleQueryCommand(char *inv[], node_t **head);
 void handleUpdateCommand(char *inv[], node_t **head);
 void handleDeleteCommand(char *inv[], node_t **head);
 void handleSaveCommand(char *inv[], node_t **head);
 
-char *rtrim(char *data)
+char *rtrim(char *data);
 
 int readFromFile(const char *filename, node_t **mainHead);
 void saveFromFile(const char *filename, node_t **head);
